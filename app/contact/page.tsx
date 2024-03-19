7a97b6dc57546048c119980fa18f807f382b3901
inoguez <sk8ian100@gmail.com>
2024-03-19 01:34:11 -0600

    Suspense boundary for useSearchParams

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,14 @@
 // import { Input } from '@/components/ui/input';
 
+import { Suspense } from 'react';
 import ContactForm from '../components/ContactForm';
 
 export default function page() {
   return (
     <main className=' container mx-auto py-5 grow flex flex-col gap-4 items-center'>
-      <ContactForm />
+      <Suspense>
+        <ContactForm />
+      </Suspense>
     </main>
   );
 }
